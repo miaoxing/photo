62099d98199f9c4673e3499ac0f0d20966963400
twinh <twinhuang@qq.com>
2018-09-04 18:47:26 +0800

    test: 实现modal自适应高度

--- resources/views/album/index.php
+++ resources/views/album/index.php
@@ -3,38 +3,38 @@
 $view->layout();
 ?>
 
-<pre><code id="c"></code></pre>
+<div id="show">
+  显示图片
+  <img class="js-a" src="">
+  <pre id="pre">
+
+  </pre>
+</div>
 
 <?= $block->js() ?>
 <script>
   require(['plugins/wechat/js/wx'], function (wx) {
     wx.load(function () {
-      wx.getLocation({
-        type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+      wx.chooseImage({
+        //sizeType: ['original'], // 可以指定是原图还是压缩图，默认二者都有
         success: function (res) {
-          var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-          var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-          var speed = res.speed; // 速度，以米/每秒计
-          var accuracy = res.accuracy; // 位置精度
-          alert(latitude);
-          alert(longitude);
-          // alert(JSON.stringify(res));
-          $('#c').html(JSON.stringify(res));
-        },
-        fail: function (res) {
-          alert('fail')
+          // {sourceType: "album"}
+          //
+
           alert(JSON.stringify(res));
 
+          var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+          $.each(localIds, function (i, localId) {
+            $('.js-a').attr('src', localId);
 
-        },
-        complete: function (res) {
-          //alert('complete')
-          //alert(JSON.stringify(res));
-        },
-        cancel: function (res) {
-          alert('cancel')
-          alert(JSON.stringify(res));
-        },
+            // wx.uploadImage({
+            //   localId: localId, // 需要上传的图片的本地ID，由chooseImage接口获得
+            //   success: function (res) {
+            //     alert(JSON.stringify(res));
+            //   }
+            // });
+          });
+        }
       });
     });
   });
